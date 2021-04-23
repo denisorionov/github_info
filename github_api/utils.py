9ede6e9008a6e56d2e6d3fa287cd9cb135f21dfa
Denis Orionov <orionovdv@gmail.com>
2021-04-23 21:37:05 +0300

    Rename variables

--- github_api/utils.py
+++ github_api/utils.py
@@ -15,22 +15,22 @@ def search(token, owner):
         return None
 
     pull_requests = response.json()
-    pr_detail = [requests.get(item["pull_request"]["url"], headers=headers).json() for item in pull_requests["items"]]
+    pull_request_detail = [requests.get(item["pull_request"]["url"], headers=headers).json() for item in pull_requests["items"]]
     result_search = {}
 
-    for pr in pr_detail:
-        if pr["base"]["repo"]["name"] not in result_search:
-            result_search[pr["base"]["repo"]["name"]] = {
-                "url": pr["base"]["repo"]["html_url"],
-                "star": pr["base"]["repo"]["stargazers_count"],
+    for pull_request in pull_request_detail:
+        if pull_request["base"]["repo"]["name"] not in result_search:
+            result_search[pull_request["base"]["repo"]["name"]] = {
+                "url": pull_request["base"]["repo"]["html_url"],
+                "star": pull_request["base"]["repo"]["stargazers_count"],
                 "pr_unmerged": [],
                 "pr_merged": []
             }
-        if pr["merged_at"] is None:
-            result_search[pr["base"]["repo"]["name"]]["pr_unmerged"].append(
-                {pr["html_url"]: {"comments": pr["comments"], "number": pr["number"]}})
+        if pull_request["merged_at"] is None:
+            result_search[pull_request["base"]["repo"]["name"]]["pr_unmerged"].append(
+                {pull_request["html_url"]: {"comments": pull_request["comments"], "number": pull_request["number"]}})
         else:
-            result_search[pr["base"]["repo"]["name"]]["pr_merged"].append(
-                {pr["html_url"]: {"comments": pr["comments"], "number": pr["number"]}})
+            result_search[pull_request["base"]["repo"]["name"]]["pr_merged"].append(
+                {pull_request["html_url"]: {"comments": pull_request["comments"], "number": pull_request["number"]}})
 
     return result_search
