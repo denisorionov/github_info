e718a46b42691af0538fd7f808e6f04cf950c694
Denis Orionov <orionovdv@gmail.com>
2021-04-24 22:27:09 +0300

    Add tests

--- github_api/tests.py
+++ github_api/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from environs import Env
 
-# Create your tests here.
+from github_api.utils import search
+
+env = Env()
+env.read_env()
+
+
+class SearchViewTests(SimpleTestCase):
+
+    def test_view(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_search(self):
+        response = self.client.get('', {"owner": "denisorionov"})
+        self.assertEqual(response.status_code, 200)
+
+    def test_utils_search_user_not_found(self):
+        owner = "*"
+        token = env.str("token")
+        search_result = search(token, owner)
+        self.assertEqual(None, search_result)
+
+    def test_utils_search_user_not_pull_requests(self):
+        owner = "denisorionov"
+        token = env.str("token")
+        search_result = search(token, owner)
+        self.assertEqual({}, search_result)
+
+    def test_utils_search_user_have_pull_requests(self):
+        owner = "Bashar"
+        token = env.str("token")
+        search_result = search(token, owner)
+        self.assertEqual(any(search_result), True)
